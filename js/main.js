82a3dc085e2a72dee5d4140a96b880e02e16723e
dr-maguigan <104376156+dr-maguigan@users.noreply.github.com>
2022-12-13 10:41:01 -0600

    Update and rename null to main.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//add map centered on Louisiana
+var map = L.map('map', {
+	center: [30.9843, -91.9623],
+	zoom: 7.5
+});
+
+//create global covid variable and retrieve information, assign it to covid
+var anchorage;
+var covid = $.ajax({
+    type: "GET",
+    url: "https://data.cdc.gov/resource/kn79-hsxy.json",
+    dataType: 'json',
+    async: false,
+    data: $(this).serialize(),
+    success: function(data) {
+	    ad = (covid[1].covid_death);
+	    anchorage = Number(ad);
+    }
+});
+console.log(anchorage);
