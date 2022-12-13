f7bbcd5684177ab5f1029221742e605ea2019daa
dr-maguigan <104376156+dr-maguigan@users.noreply.github.com>
2022-12-13 15:12:36 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //add map centered on Louisiana
 var map = L.map('map', {
 	center: [31.9843, -91.9623],
-	zoom: 7
+	zoom: 7.25
 });
 //add openstreetmap tilelayer
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
